504eaa066767b885dc760840f4b01d680bd3543e
Guilherme <guilherme.schmitt@alkasoft.com.br>
2020-09-12 10:59:45 -0300

    Initial commit

---
+++ src/main/java/br/com/searchgithubrepositories/SearchGithubRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package br.com.searchgithubrepositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchGithubRepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchGithubRepositoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/searchgithubrepositories/SearchGithubRepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.searchgithubrepositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchGithubRepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
