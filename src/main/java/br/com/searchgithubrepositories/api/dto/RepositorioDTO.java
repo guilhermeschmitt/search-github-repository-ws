3ebbc53a1ba8033a0ee05ab28a7710a215bc4888
Guilherme <guilherme.schmitt@alkasoft.com.br>
2020-09-15 09:31:16 -0300

    SGR-0001: Alterado o formato de envio das datas para string

--- src/main/java/br/com/searchgithubrepositories/api/dto/RepositorioDTO.java
+++ src/main/java/br/com/searchgithubrepositories/api/dto/RepositorioDTO.java
@@ -1,7 +1,5 @@
 package br.com.searchgithubrepositories.api.dto;
 
-import java.time.ZonedDateTime;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
@@ -16,13 +14,14 @@ public class RepositorioDTO {
 	private String full_name;
 	private UsuarioDTO owner;
 	private Long forks_count;
+	private String updated_at;
+	private String created_at;
 	private LicencaDTO license;
 	private String description;
 	private Long watchers_count;
 	private Long stargazers_count;
 	private Long subscribers_count;
 	private Long open_issues_count;
-	private ZonedDateTime updated_at;
-	private ZonedDateTime created_at;
+	
 
 }
